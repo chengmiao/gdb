7c22d320c11b04ce479457b0873318b22dfbe2d7
fatih <fatih@qq.com>
2018-06-06 20:56:27 +0800

    update

--- include/dbconnection.hpp
+++ include/dbconnection.hpp
@@ -1,21 +1,43 @@
 #pragma once 
 
-#include "mysql.h"
+#include "mysql/mysql.h"
 #include <memory>
 
 class DBConnection{
 
 	public:
 
-		DBConnection(const std::string& host , const std::string & user )
+		DBConnection(
+				const std::string & db , 
+				const std::string & user = "root",
+				const std::string & passwd = "",
+				const std::string& host  = "localhsot", 
+				int port = 3306 )
 		{
+			m_host = host; 
+			m_user = user; 
+			m_passwd = passwd; 
 
+			m_db = db; 
+			m_port = port; 
 		}
-		void init()
+
+		int init()
 		{
-			mysql_init(m_mysql);
+			mysql_init();
 			my_bool reconnect = 1;
 			mysql_options(this->m_mysql, MYSQL_OPT_RECONNECT, &reconnect);
+
+			m_mysql = mysql_real_connect(m_mysql,
+					m_host.c_str(),
+					m_user.c_str(),
+					m_passwd.c_str(),
+					m_db.c_str(),
+					m_port,
+					NULL,
+					0);
+
+
 		}
 		bool is_connected()
 		{
@@ -43,7 +65,12 @@ class DBConnection{
 	private:
 		bool m_connected; 
 
-		 MYSQL * m_mysql;
+		std::string m_host = "localhost" ; 
+		std::string m_db = ""; 
+		std::string user  = "root" ; 
+		std::string m_passwd = ""; 
+		int m_port  = 3306; 
+		MYSQL * m_mysql;
 }; 
 
 //typedef std::shared_ptr<DBConnection> DBConnectionPtr; 
