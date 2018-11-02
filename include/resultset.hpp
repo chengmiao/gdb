ae3eee0970a46816b5bae86fa2f048c1aebe59aa
huangsiyu <huangsiyu01@shandagames.com>
2018-11-02 11:18:36 +0800

    fix warning int-->uint

--- include/resultset.hpp
+++ include/resultset.hpp
@@ -1,10 +1,10 @@
-#pragma once 
+#pragma once
 #include <unordered_map>
 #include <string>
 #include <memory>
 #include "mysql.h"
 #include "logger.hpp"
-#include <stdlib.h>  
+#include <stdlib.h>
 
 
 namespace gdp
@@ -12,23 +12,23 @@ namespace gdp
     namespace db{
 
 
-        class ResultSet : public std::enable_shared_from_this<ResultSet> 
+        class ResultSet : public std::enable_shared_from_this<ResultSet>
         {
-            typedef std::unordered_map<std::string ,uint32_t > FieldMap; 
+            typedef std::unordered_map<std::string ,uint32_t > FieldMap;
 
             public:
             ResultSet(MYSQL_RES * r ):m_res(r)
             {
                 m_field_num = mysql_num_fields(m_res);
-                load_field_map(); 
+                load_field_map();
             }
             ~ResultSet()
             {
                 if (m_res)
                 {
-                    mysql_free_result(m_res); 
-                    m_res = nullptr; 
-                    m_row = nullptr; 
+                    mysql_free_result(m_res);
+                    m_res = nullptr;
+                    m_row = nullptr;
                 }
             }
 
@@ -38,157 +38,157 @@ namespace gdp
                 {
                     if (!m_row)
                     {
-                        m_row = mysql_fetch_row(this->m_res); 
+                        m_row = mysql_fetch_row(this->m_res);
                     }
                     else
                     {
-                        mysql_field_seek(this->m_res, 0); 
-                        m_row = mysql_fetch_row(this->m_res); 
+                        mysql_field_seek(this->m_res, 0);
+                        m_row = mysql_fetch_row(this->m_res);
                     }
-                    return shared_from_this(); 
+                    return shared_from_this();
                 }
-                return nullptr; 
+                return nullptr;
             }
 
-            int32_t get_int32_at(int32_t idx)
+            int32_t get_int32_at(uint32_t idx)
             {
                 if (idx >= m_field_num)
                 {
-                    elog("column index out of range"); 
-                    return 0; 
+                    elog("column index out of range");
+                    return 0;
                 }
                 if (m_row)
                 {
-                    return atoi(m_row[idx]); 
+                    return atoi(m_row[idx]);
                 }
-                return 0; 
+                return 0;
             }
 
-            int64_t get_int64_at(int32_t idx)
+            int64_t get_int64_at(uint32_t idx)
             {
                 if (idx >= m_field_num)
                 {
-                    elog("column index out of range"); 
-                    return 0; 
+                    elog("column index out of range");
+                    return 0;
                 }
                 if (m_row)
                 {
-                    return atoll(m_row[idx]); 
+                    return atoll(m_row[idx]);
                 }
-                return 0; 
+                return 0;
             }
 
             int32_t  get_int32(const std::string & key )
             {
                 if (m_row)
                 {
-                    auto field =  m_field_map.find(key); 
+                    auto field =  m_field_map.find(key);
                     if (field != m_field_map.end())
                     {
-                        uint32_t idx = field->second; 
-                        return atoi(m_row[idx]); 
+                        uint32_t idx = field->second;
+                        return atoi(m_row[idx]);
                     }
                 }
-                return 0; 
+                return 0;
             }
 
             int64_t  get_int64(const std::string & key )
             {
                 if (m_row)
                 {
-                    auto field =  m_field_map.find(key); 
+                    auto field =  m_field_map.find(key);
                     if (field != m_field_map.end())
                     {
-                        uint32_t idx = field->second; 
-                        return atoll(m_row[idx]); 
+                        uint32_t idx = field->second;
+                        return atoll(m_row[idx]);
                     }
                 }
-                return 0; 
+                return 0;
             }
 
             std::string get_string(const std::string & key )
             {
                 if (m_row)
                 {
-                    auto field =  m_field_map.find(key); 
+                    auto field =  m_field_map.find(key);
                     if (field != m_field_map.end())
                     {
-                        uint32_t idx = field->second; 
-                        return m_row[idx]; 
+                        uint32_t idx = field->second;
+                        return m_row[idx];
                     }
                 }
-                return ""; 
+                return "";
             }
 
             std::string get_string_at(uint32_t idx)
             {
                 if (idx >= m_field_num)
                 {
-                    std::cerr<< "column index out of range" << std::endl; 
-                    return 0; 
+                    std::cerr<< "column index out of range" << std::endl;
+                    return 0;
                 }
                 if (m_row)
                 {
-                    return std::string(m_row[idx]); 
+                    return std::string(m_row[idx]);
                 }
-                return ""; 
+                return "";
             }
 
             float get_float(const std::string & key)
             {
                 if (m_row)
                 {
-                    auto field =  m_field_map.find(key); 
+                    auto field =  m_field_map.find(key);
                     if (field != m_field_map.end())
                     {
-                        uint32_t idx = field->second; 
-                        return std::stof(m_row[idx]); 
+                        uint32_t idx = field->second;
+                        return std::stof(m_row[idx]);
                     }
 
                 }
-                return 0; 
+                return 0;
             }
 
             float  get_float_at(uint32_t idx)
             {
                 if (idx >= m_field_num)
                 {
-                    std::cerr<< "column index out of range" << std::endl; 
-                    return 0; 
+                    std::cerr<< "column index out of range" << std::endl;
+                    return 0;
                 }
                 if (m_row)
                 {
-                    return std::stof(m_row[idx]); 
+                    return std::stof(m_row[idx]);
                 }
-                return 0; 
+                return 0;
             }
 
             long long get_llong(const std::string & key)
             {
                 if (m_row)
                 {
-                    auto field =  m_field_map.find(key); 
+                    auto field =  m_field_map.find(key);
                     if (field != m_field_map.end())
                     {
-                        uint32_t idx = field->second; 
-                        return std::stoll(m_row[idx]); 
+                        uint32_t idx = field->second;
+                        return std::stoll(m_row[idx]);
                     }
                 }
-                return 0; 
+                return 0;
             }
 
             float  get_llong_at(uint32_t idx)
             {
                 if (idx >= m_field_num)
                 {
-                    std::cerr<< "column index out of range" << std::endl; 
-                    return 0; 
+                    std::cerr<< "column index out of range" << std::endl;
+                    return 0;
                 }
                 if (m_row)
                 {
-                    return std::stoll(m_row[idx]); 
+                    return std::stoll(m_row[idx]);
                 }
-                return 0; 
+                return 0;
             }
 
 
@@ -197,37 +197,37 @@ namespace gdp
             {
                 if (m_res)
                 {
-                    m_row = mysql_fetch_row(m_res); 
+                    m_row = mysql_fetch_row(m_res);
                     if (m_row)
                     {
-                        return true; 
+                        return true;
                     }
                 }
-                return false; 
+                return false;
             }
 
             my_ulonglong count()
             {
-                return mysql_num_rows(m_res); 
+                return mysql_num_rows(m_res);
             }
 
             private:
             void load_field_map()
             {
                 uint32_t num  = mysql_num_fields(m_res);
-                MYSQL_FIELD * fields = mysql_fetch_fields(m_res); 
+                MYSQL_FIELD * fields = mysql_fetch_fields(m_res);
                 for(uint32_t i =0; i < num ; i ++)
                 {
-                    m_field_map[fields[i].name ]  = i ; 
+                    m_field_map[fields[i].name ]  = i ;
                 }
             }
 
             MYSQL_RES* m_res  = nullptr;
-            MYSQL_ROW  m_row  = nullptr; 
-            FieldMap m_field_map; 
-            uint32_t m_field_num = 0; 
-        }; 	
+            MYSQL_ROW  m_row  = nullptr;
+            FieldMap m_field_map;
+            uint32_t m_field_num = 0;
+        };
 
-        typedef std::shared_ptr<ResultSet> ResultSetPtr; 
+        typedef std::shared_ptr<ResultSet> ResultSetPtr;
     }
 }
